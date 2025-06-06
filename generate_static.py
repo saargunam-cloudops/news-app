005878462c268beb5fc71e9e8c239b1a1ef22b23
Saargunam <102005772+saargunam-cloudops@users.noreply.github.com>
2025-06-07 01:32:30 +0530

    News app v2 - github pages - issue fix

--- generate_static.py
+++ generate_static.py
@@ -1,11 +1,13 @@
-from news_dashboard import index
+from news_dashboard import app, index
 import os
 
 output_dir = "output"
 os.makedirs(output_dir, exist_ok=True)
 
-html_content = index()
+with app.app_context():
+    html_content = index()
+
 with open(os.path.join(output_dir, "index.html"), "w", encoding="utf-8") as f:
     f.write(html_content)
 
-print("Static HTML generated in /output/index.html")
+print("✅ Static HTML generated in /output/index.html")
